87f3bc3498200a76058d73c7625f4490983db124
fivedollarfridays <you@fivedollarfridays.com>
2025-10-08 23:40:20 -0500

    feat: Add global fetch mock to jest.setup.js - Phase 1 Sprint 13

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,28 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Global fetch mock for all tests
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    status: 200,
+    statusText: 'OK',
+    json: () => Promise.resolve({}),
+    text: () => Promise.resolve(''),
+    blob: () => Promise.resolve(new Blob()),
+    arrayBuffer: () => Promise.resolve(new ArrayBuffer(0)),
+    formData: () => Promise.resolve(new FormData()),
+    headers: new Headers(),
+    redirected: false,
+    type: 'basic',
+    url: '',
+    clone: function() { return this; },
+    body: null,
+    bodyUsed: false,
+  })
+);
+
+// Reset fetch mock before each test
+beforeEach(() => {
+  global.fetch.mockClear();
+});
